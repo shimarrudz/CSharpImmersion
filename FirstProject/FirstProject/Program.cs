7c22489d7c5ff7787bff1316d48ad4270fd4e448
Victor Shimada <vic.shima.vss@gmail.com>
2024-04-02 12:17:24 -0300

    docs: add constructors file

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace FirstProject {
+    internal class Program {
+        static void Main(string[] args) {
+
+
+            sbyte x = 100;
+
+            Console.WriteLine(x);
+        }
+    }
+}
